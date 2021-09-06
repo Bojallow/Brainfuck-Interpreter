67ad223d6295d9642b5f43431f351fe620ebfd18
Bocar Jallow <82125366+Bojallow@users.noreply.github.com>
2021-09-06 16:08:40 -0400

    Add files via upload

---
+++ Env.cs
@@ -0,0 +1,16 @@
+﻿namespace Env {
+    unsafe class Environment {
+        private char[] memory = new char[0x752F];
+        private char memPtr;
+        char* beginning;
+
+
+        Environment() {
+            memPtr = memory[0];
+            char temp = memory[0];
+            beginning = &temp;
+        }
+
+        
+    }
+}
