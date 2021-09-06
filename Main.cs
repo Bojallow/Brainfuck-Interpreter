23871bf0249761c4c2ca6e7248e4c613fe44e5d9
Bocar Jallow <82125366+Bojallow@users.noreply.github.com>
2021-09-06 16:07:40 -0400

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,19 @@
+﻿namespace Interpreter {
+
+    class Brainfuck {
+
+        public static void Main(string[] args) {
+            string filename = "";
+            System.Console.WriteLine("Where is the location and name of your file? ");
+            filename = System.Console.ReadLine();
+
+            if(filename.Substring(filename.Length - 3) != ".bf") {
+                System.Console.WriteLine("Error:");
+                System.Console.WriteLine("code exited with error code 6: File must be a Brainfuck (.bf) file.");
+            }
+        }
+    }
+
+       
+
+}
