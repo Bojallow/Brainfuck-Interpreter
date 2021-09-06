97e686347c96db43c8f36458506573dcfe400b0e
Bocar Jallow <82125366+Bojallow@users.noreply.github.com>
2021-09-06 16:06:40 -0400

    Add files via upload

---
+++ Interpreter.cs
@@ -0,0 +1,29 @@
+﻿namespace Inter {
+
+    class Interpreter {
+        private string Code;
+
+        Interpreter(string filename) {
+            Code = System.IO.File.ReadAllText(filename);
+        } 
+
+        public void Main() {
+            foreach(char ii in Code)
+            {
+                switch (ii) {
+                    case '\n':
+                        continue;
+                    case '\t':
+                        continue;
+                    case ' ':
+                        continue;
+                    default:
+                        return;
+
+                }
+
+            }
+        }
+    }
+   
+}
